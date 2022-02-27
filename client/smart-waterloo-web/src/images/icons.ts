ece802d16e4aa66990bb49f37e8bd1e74a9753ca
Zac Waite <129032699zw@gmail.com>
2022-02-26 20:33:55 -0500

    Assets

---
+++ client/smart-waterloo-web/src/images/icons.ts
@@ -0,0 +1,12 @@
+import menuIcon from "./menu.svg";
+import checkIcon from "./check.svg";
+import closeIcon from "./close.svg";
+import editIcon from "./edit.svg";
+import rightArrowIcon from "./rightArrow.svg";
+export const icons = {
+	menu: menuIcon,
+	check: checkIcon,
+	close: closeIcon,
+	edit: editIcon,
+	rightArrow: rightArrowIcon
+}
