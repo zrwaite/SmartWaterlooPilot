3437d312234af8e2906d64f1fd8c42fc4b64f6c1
DjKesu <km67000@yahoo.com>
2022-03-12 18:00:59 -0500

    Errors with communication with the smart contract

--- client/smart-waterloo-web/src/pages/SignUp/Profile/FormGrid/FormGrid.tsx
+++ client/smart-waterloo-web/src/pages/SignUp/Profile/FormGrid/FormGrid.tsx
@@ -1,17 +1,52 @@
 // import {Link} from "react-router-dom";
-import {genderOptions, religionOptions, sexualityOptions, raceOptions, ProfileFormGridState} from "./FormGridData";
-// import {useState} from "react";
-import Select, {ActionMeta} from "react-select";
+import { genderOptions, religionOptions, sexualityOptions, raceOptions, ProfileFormGridState } from "./FormGridData";
+import {useEffect, useState} from "react";
+import Select, { ActionMeta } from "react-select";
+import userABI from "../../utils/SmartUser.json"
+import { ethers } from "ethers";
+
+declare var window: any;
+
 type ProfileFormGridProps = {
 	updateStep: Function,
-	handleParentInputChange: (event: React.ChangeEvent<HTMLInputElement>|React.ChangeEvent<HTMLSelectElement>) => void,
-	handleParentSelectChange:  (newValue: null|{ value: string; label: string; }, actionMeta: ActionMeta<{value: string,label: string}>) => void,
+	handleParentInputChange: (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void,
+	handleParentSelectChange: (newValue: null | { value: string; label: string; }, actionMeta: ActionMeta<{ value: string, label: string }>) => void,
 	formData: ProfileFormGridState
 }
-function ProfileFormGrid(props:ProfileFormGridProps) {
-	let redText = {color: "red"};
-	let greyText = {color: "grey"};
-    return (
+function ProfileFormGrid(props: ProfileFormGridProps) {
+	let redText = { color: "red" };
+	let greyText = { color: "grey" };
+
+	const [userAddress, setUserAddress] = useState([]);
+	useEffect(() => {
+		if(window.ethereum){
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then((accounts: []) => setUserAddress(accounts));
+		}
+	});
+
+	//User Information Smart Contract
+	const contractAddress = "0xE51426A3e2e793B740A95073C6598e82fF64A09a";
+	const contractABI = userABI.abi;
+	const onSubmit = () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const userContract = new ethers.Contract(
+			contractAddress,
+			contractABI,
+			signer
+		);
+		
+		console.log(userAddress[0],(props.formData.day + props.formData.month + props.formData.year), props.formData.gender, props.formData.height, props.formData.weight, props.formData.grade, props.formData.postalCode, props.formData.race, props.formData.religion, props.formData.sexuality);
+
+		userContract.addInfo(provider, (props.formData.day + props.formData.month + props.formData.year), props.formData.gender, props.formData.height, props.formData.weight, props.formData.grade, props.formData.postalCode, props.formData.race, props.formData.religion, props.formData.sexuality);
+		// //Just trying to retrieve information from the smart contrsct, so testing using the getTotalUsers function
+		const d = userContract.getInfo(userAddress[0]);
+		console.log(d);
+		// props.updateStep(3);
+	};
+
+	return (
 		<>
 			<main>
 				<section className="formGrid">
@@ -23,18 +58,18 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 							Date of Birth<span style={redText}>*</span>
 						</p>
 						<div className="horizontal">
-							<input name="day" id="dayInput" placeholder="DD" type={"text"} value={props.formData.day} onChange={props.handleParentInputChange}/>
+							<input name="day" id="dayInput" placeholder="DD" type={"text"} value={props.formData.day} onChange={props.handleParentInputChange} />
 							<pre> / </pre>
-							<input name="month" id="monthInput" placeholder="MM"  type={"text"} value={props.formData.month} onChange={props.handleParentInputChange}/>
+							<input name="month" id="monthInput" placeholder="MM" type={"text"} value={props.formData.month} onChange={props.handleParentInputChange} />
 							<pre> / </pre>
-							<input name="year" id="yearInput" placeholder="YY"  type={"text"} value={props.formData.year} onChange={props.handleParentInputChange}/>
+							<input name="year" id="yearInput" placeholder="YY" type={"text"} value={props.formData.year} onChange={props.handleParentInputChange} />
 						</div>
 					</div>
 					<div className="formDiv">
 						<p>
 							Gender<span style={redText}>*</span>
 						</p>
-						<Select className={"selectComponent"} defaultInputValue={props.formData.gender} name={"gender"} onChange={props.handleParentSelectChange} options={genderOptions}/>
+						<Select className={"selectComponent"} defaultInputValue={props.formData.gender} name={"gender"} onChange={props.handleParentSelectChange} options={genderOptions} />
 					</div>
 					<div className="formDiv horizontal">
 						<div>
@@ -42,7 +77,7 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 								Height<span style={redText}>*</span>
 							</p>
 							<div className="pill">
-								<input name="height" id="heightInput" placeholder="168" type={"text"} value={props.formData.height} onChange={props.handleParentInputChange}/>
+								<input name="height" id="heightInput" placeholder="168" type={"text"} value={props.formData.height} onChange={props.handleParentInputChange} />
 								<p className="pillText">cm</p>
 							</div>
 						</div>
@@ -51,7 +86,7 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 								Weight<span style={redText}>*</span>
 							</p>
 							<div className="pill">
-								<input name="weight" id="weightInput" placeholder="160" type={"text"} value={props.formData.weight} onChange={props.handleParentInputChange}/>
+								<input name="weight" id="weightInput" placeholder="160" type={"text"} value={props.formData.weight} onChange={props.handleParentInputChange} />
 								<p className="pillText">lbs</p>
 							</div>
 						</div>
@@ -60,19 +95,19 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 						<p>
 							Religion <span style={greyText}>(Optional)</span>
 						</p>
-						<Select className={"selectComponent"} defaultInputValue={props.formData.religion} name={"religion"} onChange={props.handleParentSelectChange} options={religionOptions}/>
+						<Select className={"selectComponent"} defaultInputValue={props.formData.religion} name={"religion"} onChange={props.handleParentSelectChange} options={religionOptions} />
 					</div>
 					<div className="formDiv">
 						<p>
 							Sexuality <span style={greyText}>(Optional)</span>
 						</p>
-						<Select className={"selectComponent"} defaultInputValue={props.formData.sexuality} name={"sexuality"} onChange={props.handleParentSelectChange} options={sexualityOptions}/>
+						<Select className={"selectComponent"} defaultInputValue={props.formData.sexuality} name={"sexuality"} onChange={props.handleParentSelectChange} options={sexualityOptions} />
 					</div>
 					<div className="formDiv">
 						<p>
 							Race <span style={greyText}>(Optional)</span>
 						</p>
-						<Select className={"selectComponent"} defaultInputValue={props.formData.race} name={"race"} onChange={props.handleParentSelectChange} options={raceOptions}/>
+						<Select className={"selectComponent"} defaultInputValue={props.formData.race} name={"race"} onChange={props.handleParentSelectChange} options={raceOptions} />
 					</div>
 					<div className="formDiv horizontal">
 						<div>
@@ -82,7 +117,7 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 							<div className="horizontal">
 								<div className="pill">
 									<p className="pillText">-</p>
-									<input name="grade" id="gradeInput" type={"text"} value={props.formData.grade} onChange={props.handleParentInputChange}/>
+									<input name="grade" id="gradeInput" type={"text"} value={props.formData.grade} onChange={props.handleParentInputChange} />
 									<p className="pillText">+</p>
 								</div>
 							</div>
@@ -91,18 +126,18 @@ function ProfileFormGrid(props:ProfileFormGridProps) {
 							<p>
 								Postal Code<span style={redText}>*</span>
 							</p>
-							<input name="postalCode" id="postalCodeInput" placeholder="M5A" type={"text"} value={props.formData.postalCode} onChange={props.handleParentInputChange}/>
+							<input name="postalCode" id="postalCodeInput" placeholder="M5A" type={"text"} value={props.formData.postalCode} onChange={props.handleParentInputChange} />
 						</div>
 					</div>
 				</section>
 				<div className="formDiv">
-					<button className="blackButton signUpButton" onClick={() => props.updateStep(3)}>
+					<button className="blackButton signUpButton" onClick={onSubmit}>
 						Continue
 					</button>
 				</div>
 			</main>
 		</>
-    );
+	);
 }
 
 export default ProfileFormGrid;
