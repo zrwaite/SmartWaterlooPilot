29f31f950770110f847858456170751f7e60bb51
songemma <xemmasongx@gmail.com>
2022-03-13 12:13:39 -0400

    Add popup window for EventDetails page

--- client/smart-waterloo-web/src/pages/EventDetails/EventDetails.tsx
+++ client/smart-waterloo-web/src/pages/EventDetails/EventDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {icons} from "../../images/icons";
 import {event_images} from "../../images/eventimages";
+import img_party_popper from "../../images/emoji-party-popper.png"
 // import Sidebar from "../../components/Sidebar";
 import Navbar from "../../components/Navbar";
 import { MobileContext } from "../../App";
@@ -17,6 +18,9 @@ import {
 	useParams,
 	useNavigate,
   } from "react-router-dom";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const EventsDetails = () => {
 	let {mobile} = useContext(MobileContext);
@@ -25,13 +29,35 @@ const EventsDetails = () => {
 	const [buttonText, setText] = React.useState("Sign Up");
 	const [signupButtonClass, setClass] = React.useState("signupButton");
 	const [bottomButtonClass, setBottomClass] = React.useState("bottomButton");
+	const [isOpen, setIsOpen] = React.useState(false);
 
 	const event = eventDataRaw.find(event => event.name === name);
 
 	if (!event) return <NotFound />;
 
+	function openModal() {
+		setIsOpen(true);
+	  }
+
+	function closeModal() {
+		setIsOpen(false);
+	}
+
 	return (
 		<>
+			<Modal isOpen={isOpen} onRequestClose={closeModal} className="popupPanel">
+				<div className='messagePanel'>
+					<div>
+						<img src={img_party_popper} alt="Yay! You're In!" className="h5"/>
+					</div>
+					<div>
+						<h6 className='popupMessage'>Yay! You're in! </h6>
+					</div>
+					<div>
+						<p className="popupButton" onClick={() => closeModal() }>OK</p>
+					</div>
+				</div>
+			</Modal>	
 			<div className='navbarEventDetails'>
 				<div className='leftNavEventDetail'>
 					<div onClick={() => navigate("../Events")} className="eventBackButton">
@@ -40,24 +66,28 @@ const EventsDetails = () => {
 					</div>					
 				</div>
 				<div>
-					<p className={signupButtonClass} onClick={() => { setText("Signed Up ✓") 
+					<p className={signupButtonClass} onClick={() => { 
+					openModal()
+					setText("Signed Up ✓") 
 					setClass("signupLightBlueButton")
 					setBottomClass("bottomLightBlueButton") }}>{buttonText}</p>
-				</div>				
+				</div>		
 			</div>
 			<div className={"PageContainer"}>
 				<div className={mobile? "":"DesktopPanelNoPadding"}>
 					<div className={"eventDetails"}>
 						<img src={event_images.basketball_skills} alt={event.title} className="eventImage" />
 					</div>
-					<EventInfo name={event.name} text={buttonText} class={signupButtonClass} bottomClass={bottomButtonClass}/>
+					<EventInfo name={event.name} />
 					<div className="DesktopPanelNoBorder">
-						<p className={bottomButtonClass} onClick={() => { setText("Signed Up ✓") 
+						<p className={bottomButtonClass} onClick={() => { 
+						openModal()
+						setText("Signed Up ✓") 
 						setClass("signupLightBlueButton")
 						setBottomClass("bottomLightBlueButton") }}>{buttonText}</p>
 					</div>
 				</div>
-			</div>	
+			</div>
 		</>
 	);
 }
