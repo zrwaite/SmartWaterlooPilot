7cdedbe4fb9d390c022d06ce7e76353c4749a0d9
Zac Waite <129032699zw@gmail.com>
2022-05-17 22:07:20 -0400

    OrgData User Info

--- client/smart-waterloo-web/src/pages/OrgData/OrgData.tsx
+++ client/smart-waterloo-web/src/pages/OrgData/OrgData.tsx
@@ -7,73 +7,22 @@ import { defaultOrg } from "../../data/types/orgs";
 import { useParams } from "react-router-dom";
 import { getDefaultUserInfoLists } from "../../data/types/account";
 import {USE_WEB3} from "../../data/dataConstants";
+import UserInfo from "../../components/UserInfo";
 const OrgData = (props:AccountChildProps) => {
 	let {orgId} = useParams();
 	let {mobile} = useContext(MobileContext);
 	const [orgData, setOrgData] = useState({org: defaultOrg, set: false});
-	const [userInfoParsed, setUserInfoParsed] = useState(false);
-	const [userInfoLists, setUserInfoLists] = useState(getDefaultUserInfoLists())
-
 	if (!orgData.set) {
 		const newOrgData = props.orgsData.orgs.find(org => org.id.toString() === orgId)
 		if (newOrgData) setOrgData({org:newOrgData, set: true});
 	}
 
-	const incrementMap = (map: Map<string, number>, key:string) => {
-		let numValues = map.get(key)||0;
-		map.set(key, numValues+1);
-	}
-
-	const parseUserInfoLists = () => {
-		const newUserInfoLists = getDefaultUserInfoLists();
-		console.log(orgData.org.user_info);
-		orgData.org.user_info.forEach((user) => {
-			const age = Math.floor(((new Date()).getTime() - (new Date(user.birth_day)).getTime()) / (1000*60*60*24*365));
-			incrementMap(newUserInfoLists.ages, age.toString());
-			incrementMap(newUserInfoLists.genders, user.gender);
-			incrementMap(newUserInfoLists.races, user.race);
-			incrementMap(newUserInfoLists.religions, user.religion);
-			incrementMap(newUserInfoLists.sexualities, user.sexuality);
-		})
-		setUserInfoLists(newUserInfoLists);
-	}
 
 	if (!orgData.set) {
 		const newOrgData = USE_WEB3?props.orgsData.orgs[0]:props.orgsData.orgs.find(org => org.id.toString() === orgId)
 		if (newOrgData) setOrgData({org:newOrgData, set: true});
-	}else if (!userInfoParsed) {
-		parseUserInfoLists();
-		setUserInfoParsed(true);
 	}
 
-	let userInfoComponents:{
-		religions: JSX.Element[]
-		genders: JSX.Element[]
-		races: JSX.Element[]
-		ages: JSX.Element[]
-		sexualities: JSX.Element[]
-	}= {
-		religions: [],
-		genders: [],
-		races: [],
-		ages: [],
-		sexualities: []
-	}
-	userInfoLists.ages.forEach((value, key) => {
-		userInfoComponents.ages.push(<p>{key}: {value}</p>)
-	})
-	userInfoLists.religions.forEach((value, key) => {
-		userInfoComponents.religions.push(<p>{key}: {value}</p>)
-	})
-	userInfoLists.sexualities.forEach((value, key) => {
-		userInfoComponents.sexualities.push(<p>{key}: {value}</p>)
-	})
-	userInfoLists.races.forEach((value, key) => {
-		userInfoComponents.races.push(<p>{key}: {value}</p>)
-	})
-	userInfoLists.genders.forEach((value, key) => {
-		userInfoComponents.genders.push(<p>{key}: {value}</p>)
-	})
 	return (
 		<>
 			<Navbar root={false}/>
@@ -84,29 +33,7 @@ const OrgData = (props:AccountChildProps) => {
 					<p>Business Number: {orgData.org.business_number}</p>
 					<p>Verified: {Boolean(orgData.org.verified).toString()}</p>
 					<p>Num Members: {orgData.org.members.length+1}</p>
-					{<>
-						<h6>Member info:</h6>
-						<p>Ages:</p>
-						<ul>
-							{userInfoComponents.ages.map((component, key) => <li key={key}>{component}</li>)}
-						</ul>
-						<p>Genders:</p>
-						<ul>
-							{userInfoComponents.genders.map((component, key) => <li key={key}>{component}</li>)}
-						</ul>
-						<p>Religions:</p>
-						<ul>
-							{userInfoComponents.religions.map((component, key) => <li key={key}>{component}</li>)}
-						</ul>
-						<p>Sexual Orientations:</p>
-						<ul>
-							{userInfoComponents.sexualities.map((component, key) => <li key={key}>{component}</li>)}
-						</ul>
-						<p>Races:</p>
-						<ul>
-							{userInfoComponents.races.map((component, key) => <li key={key}>{component}</li>)}
-						</ul>
-					</>}
+					<UserInfo userInfo={orgData.org.user_info} dataParsed={props.doneParsing}/>
 				</div>
 			</div>	
 		</>
